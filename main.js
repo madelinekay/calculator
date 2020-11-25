75d58c921aa66cb0b18d92c55eca0a8d3046da43
Madeline Undis <mundis@smith.edu>
2020-11-24 22:30:50 -0800

    calculator initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+input = ""
+function myFunction(event) {
+    if (event.target.id=="=") {
+        calculate(input);
+    } else if (event.target.id=="c") {
+        input = ""
+        document.getElementById("js-result").innerHTML=input;
+    } else if (event.target.id=="arrow") {
+        input = input.slice(0,-1)
+        document.getElementById("js-result").innerHTML=input;
+    }
+    else { 
+        var target = event.target.id;
+        input += target;
+        document.getElementById("js-result").innerHTML=input;
+    }
+};
+
+function calculate(input) {
+    answer=eval(input)
+    document.getElementById("js-result").innerHTML=answer;
+};
+
